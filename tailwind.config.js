9bf8da121547406278d1a0b5e339d4a495f56ae1
Tukloy <rheyvince.26@gmail.com>
2025-05-26 17:01:19 +0800

    Add template

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+       fontFamily: {
+        bromello: ['bromello', 'cursive'], // fallback to cursive
+        adelia: ['adelia', 'cursive'],
+        photograph: ['photograph-signature', 'cursive'],
+      },
+      colors: {
+        primary: '#4B5563',
+        ivory: '#fffff0'
+      }
+    },
+  },
+  plugins: [],
+}
+
