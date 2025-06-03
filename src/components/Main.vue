2568d413b1438e5e3fe4e8396de4252328e4dbf7
Tukloy <rheyvince.26@gmail.com>
2025-06-03 12:17:45 +0800

    clean template

---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+<template>
+    <div class="h-screen bg-ivory">
+        <p class="text-primary">this is the header</p>
+        <p class="text-primary text-6xl font-bromello">Save the Date</p>
+        <p class="text-primary text-6xl font-adelia">Save the Date</p>
+        <p class="text-primary text-6xl font-photograph">Save the Date</p>
+        <p class="text-primary text-6xl font-cinzel">Save the Date</p>
+    </div>
+</template>
